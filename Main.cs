34eae125eb177ed26bd923ef669bf96125b68076
Ivan <mne21z@gmail.com>
2023-03-19 21:55:21 +0700

    dev: sheet reader protype

---
+++ Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime;
+using Multicad.Geometry;
+using Multicad.Runtime;
+using Multicad.Symbols.Tables;
+using Multicad.DataServices;
+using Multicad.Objects;
+using SpdsObjBySheetInfo;
+
+namespace NCadCustom
+{
+    public class Commands : IExtensionApplication
+    {
+        public void Initialize()
+        {
+        }
+        public void Terminate()
+        {
+        }
+
+        [CommandMethod("CreateSpdsBySh", CommandFlags.NoCheck | CommandFlags.NoPrefix)]
+        public static void MainCreateBySpreadSheet()
+        {
+
+        }
+    }
+}
---
+++ ShWorker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace SpdsObjBySheetInfo
+{
+    internal class ShWorker
+    {
+        private readonly string wbFullPath;
+        private readonly string sheetName;
+
+        public ShWorker(string shPath , string sheetName) 
+        {
+            this.wbFullPath = shPath;
+            this.sheetName = sheetName;
+            ReadDoc();
+        }
+
+        private void ReadDoc()
+        {
+            using (SpreadsheetDocument doc = SpreadsheetDocument.Open(wbFullPath, false))
+            {
+                WorkbookPart wbPart = doc.WorkbookPart;
+                Sheet sh = wbPart.Workbook.Descendants<Sheet>().FirstOrDefault(s => s.Name == sheetName);
+
+                if (sh == null)
+                {
+                    throw new ArgumentException(sheetName);
+                }
+
+                Row row = sh.Descendants<Row>().LastOrDefault();
+
+
+            }
+        }
+    }
+}
